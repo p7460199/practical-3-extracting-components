0b3b9f1edc30669b4d926ff61fbf8226c45dde75
Jeremiah Ang <jeremiahang94@gmail.com>
2023-06-13 07:56:36 +0000

    Add hobby test

---
+++ __tests__/Hobby.test.js
@@ -0,0 +1,10 @@
+import { fireEvent, render } from '@testing-library/react';
+import Hobby from '../src/Hobby.js';
+
+it('Should render emoji and title', async () => {
+    const emoji = '🏊';
+    const title = 'Swim';
+
+    const renderResult = render(<Hobby emoji={emoji} title={title} />);
+    expect(await renderResult.findByText(`${emoji} ${title}`)).toBeTruthy();
+});

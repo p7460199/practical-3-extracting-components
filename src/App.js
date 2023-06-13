c880b038e7ba18b0c43d3c19b84c1c0c84b7e7e8
Jeremiah Ang <jeremiahang94@gmail.com>
2023-06-13 06:46:48 +0000

    Add practical-2 solution

--- src/App.js
+++ src/App.js
@@ -1,3 +1,28 @@
+import Hello from './Hello.js';
+
 export default function App() {
-    return <h1>Hello World</h1>;
+    const name = 'ENTER YOUR NAME';
+    return (
+        <div>
+            <Hello name={name}></Hello>
+            <div>
+                <ol>
+                    <li>
+                        <b>Name:</b> {name}
+                    </li>
+                    <li>
+                        <b>Age:</b> 18
+                    </li>
+                    <li>
+                        <b>Hobbies:</b>
+                        <ol>
+                            <li>Swim</li>
+                            <li>Run</li>
+                            <li>Game</li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    );
 }
---
+++ src/Hello.js
@@ -0,0 +1,3 @@
+export default function Hello() {
+    return <h1>Hello, this is a page about jeremiah ang</h1>;
+}
